83dac1c2d41e4d1a5bda1f1d5d897460eacc7b2d
Leeyah-123 <junaidaaliyah260@gmail.com>
2025-06-30 18:42:54 +0100

    Add health route

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,19 @@ use dotenv::dotenv;
 use std::env;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-// use crate::keypair::routes as keypair_routes;
+use crate::keypair::routes as keypair_routes;
 use crate::message::routes as message_routes;
 use crate::response::ApiResponse;
 use crate::send::routes as send_routes;
 use crate::token::routes as token_routes;
+
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().json(ApiResponse {
+        success: true,
+        data: "Solana HTTP Server is running".to_string(),
+    })
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -31,7 +39,7 @@ async fn main() -> std::io::Result<()> {
         .init();
 
     let port = env::var("PORT")
-        .unwrap_or_else(|_| "5000".to_string())
+        .unwrap_or_else(|_| "8080".to_string())
         .parse::<u16>()
         .unwrap();
     let bind_address = env::var("BIND_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
@@ -46,10 +54,10 @@ async fn main() -> std::io::Result<()> {
             .allow_any_header()
             .max_age(3600);
 
-        // Register our app routes
+        // Register our app routes, services and middleware
         App::new()
             .wrap(cors)
-            // .configure(keypair_routes::configure)
+            .configure(keypair_routes::configure)
             .configure(token_routes::configure)
             .configure(message_routes::configure)
             .configure(send_routes::configure)
